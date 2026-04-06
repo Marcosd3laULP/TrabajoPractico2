62ea9168c0f381ea70182fc7a9c875f0549a5af5
MarcosBR--ULP2024🦈(☞ﾟヮﾟ)☞☜(ﾟヮﾟ☜) <usuario4ltnum1@gmail.com>
2026-04-05 21:07:58 -0300

    Trabajo Practico 2 Terminado

--- app/src/main/java/com/studios91/trabajopractico2/BreadcastReceiver.java
+++ app/src/main/java/com/studios91/trabajopractico2/BreadcastReceiver.java
@@ -10,23 +10,14 @@ import android.widget.Toast;
 public class BreadcastReceiver extends android.content.BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
 
+        Intent llamada = new Intent(Intent.ACTION_DIAL);
+        llamada.setData(Uri.parse("tel:2664618598"));
+        Log.d("bread", "Desbloqueo detectado");
+        llamada.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-            if (context.checkSelfPermission(android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(context, "pantalla desbloqueada detectada", Toast.LENGTH_SHORT).show();
-                Log.d("BreadCast", "Detectado desbloqueo de pantalla");
-
-                Intent llamada = new Intent(Intent.ACTION_CALL);
-                llamada.setData(Uri.parse("tel:2664618598"));
-
-                llamada.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                context.startActivity(llamada);
-
-            }
-
-        }
+        context.startActivity(llamada);
 
     }
+
 }
--- app/src/main/java/com/studios91/trabajopractico2/MainActivity.java
+++ app/src/main/java/com/studios91/trabajopractico2/MainActivity.java
@@ -15,9 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     private BreadcastReceiver receiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,27 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         receiver = new BreadcastReceiver();
 
-
-        if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 100);
-        }
-
-
         registerReceiver(receiver, new IntentFilter(Intent.ACTION_USER_PRESENT));
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 100) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permiso de llamada concedido", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Permiso denegado. No se podrá llamar automáticamente.", Toast.LENGTH_LONG).show();
-            }
-        }
     }
 
 }
